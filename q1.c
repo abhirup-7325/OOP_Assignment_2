8b3f7da686a5879d8876755e88ae470c35d1ecbf
abhirup-7325 <palabhirup07032005@gmail.com>
2024-09-04 02:36:43 +0530

    add and display functions tested

--- q1.c
+++ q1.c
@@ -8,23 +8,82 @@
 struct student {
     int roll;
     char name[101];
-    int score[5];
+    int marks[5];
+    bool isDeleted;
+};
+
+
+bool found(int roll) {
+    FILE *fp = fopen(FILENAME, "rb");
+    if (fp == NULL) {
+        return 0;
+    }
+
+    struct student s;
+    while (fread(&s, sizeof(struct student), 1, fp)) {
+        if (s.roll == roll && !s.isDeleted) {
+            fclose(fp);
+            return true;
+        }
+    }
+
+    fclose(fp);
+    return false;
 }
 
 
 void addStudent() {
+    struct student s;
+    printf("Enter roll number: ");
+    scanf("%d", &(s.roll));
+
+    if (found(s.roll)) {
+        printf("Roll number all ready exists!\n");
+        return;
+    }
+
     FILE *fp = fopen(FILENAME, "ab");
     if (fp == NULL) {
         printf("Error opening file.\n");
         return;
     }
 
-    
+    printf("Enter name: ");
+    scanf("%s", s.name);
+
+    for (int i = 0; i < 5; i++) {
+        printf("Enter marks in subject %d: ", i + 1);
+        scanf("%d", &((s.marks)[i]));
+    }
+
+    s.isDeleted = false;
+
+    fwrite(&s, sizeof(struct student), 1, fp);
+    fclose(fp);
 }
 
 
 void displayAll() {
+    FILE *fp = fopen(FILENAME, "rb");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
 
+    struct student s;
+    int totalScore = 0;
+    while (fread(&s, sizeof(struct student), 1, fp)) {
+        for (int i = 0; i < 5; i++) {
+            totalScore += (s.marks)[i];
+        }
+
+        if (!s.isDeleted) {
+            printf("Roll: %d, Name: %s, Total Score: %d\n",
+                   s.roll, s.name, totalScore);
+        }
+    }
+
+    fclose(fp);
 }
 
 
@@ -59,7 +118,7 @@ int main() {
         printf("4. Edit Record\n");
         printf("5. Delete Record (Logical)\n");
         printf("6. Delete Record (Physical)\n");
-        printf("Any other number to exit")
+        printf("Any other number to exit\n");
         
         printf("Enter your choice: ");
         scanf("%d", &response);
